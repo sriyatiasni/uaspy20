74827b1c3e08f087a1d1e51d9c22f57614409e43
sriyatiasni <56379905+sriyatiasni@users.noreply.github.com>
2020-01-13 09:39:42 +0700

    main.py

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
-class MainApp():
-    def __init__(self):
+from core.search_helper import SearchHelper
+from core.baseapp import BaseApp
+from view.view_book import ViewBook
+from view.input_book import InputBook
+
+
+class MainApp(BaseApp):
+    def _init_(self):
         self.books = []
 
 
-if __name__ == "__main__":
+if name == "_main_":
     app = MainApp()
     app.run()
 
+
+def list_book(self):
+    view = ViewBook(self.books)
+    view.list()
+
+
+def add_book(self):
+    add = InputBook()
+    add.input()
+
+
+def search_book():
+    cari = InputBook()
+    cari.search()
+    help = SearchHelper()
+    help.search_title()
+    liat = ViewBook(books=self)
+    liat.list(
